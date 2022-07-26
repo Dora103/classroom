db94819524c0abbc9cf32ba78651ba497fee48f4
ashwiniuttam3197 <ashwinibhoyar103@gmail.com>
2022-07-26 11:05:21 +0530

    length() method of String

---
+++ Strings/LengthMethod.java
@@ -0,0 +1,10 @@
+class LengthMethod
+{
+	public static void main(String[] args)
+	{
+	 String s = "Babulal";
+	 int i = s.length();
+	 System.out.println("length = "+i);
+	 System.out.println("length = "+s.length());
+	}
+}

2a2e856c0e0f4f5c55469f4cdd977a07f7d7f777
ashwiniuttam3197 <ashwinibhoyar103@gmail.com>
2022-07-26 11:07:08 +0530

    different constructors of String class

---
+++ Strings/Test.java
@@ -0,0 +1,15 @@
+class Test
+{
+	public static void main(String[] args)
+	{
+		Test t1 = new Test();
+		Test t2 = new Test();
+		System.out.println(t1 == t2);
+		String str1 = "Ashish";
+		String str2 = "Ashish";
+		System.out.println(str1 == str2);
+		String s1 = new String("help4code");
+		String s2 = new String("help4code");
+		System.out.println(s1 == s2);
+	}
+}

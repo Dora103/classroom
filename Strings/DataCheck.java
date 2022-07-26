155fdcd77e39367cd10c99c7eabc8ccf673aa999
ashwiniuttam3197 <ashwinibhoyar103@gmail.com>
2022-07-26 11:00:51 +0530

    append method in String and StringBuffer

---
+++ Strings/DataCheck.java
@@ -0,0 +1,11 @@
+class DataCheck{
+	
+	public static void main(String[] args)
+	{
+	   //String a = new String("Ashish");
+	  StringBuffer a = new StringBuffer("Rishi");
+	  //StringBuffer b = new StringBuffer("Hii");
+	  a.append("hii");
+	   System.out.println(a);
+	}
+}
